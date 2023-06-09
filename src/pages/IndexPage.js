7a18b4d607af580659aa7867ef724ee6b21e3544
dhtjddls <dhtjddls12@naver.com>
2023-06-09 15:26:04 +0900

    fix: 검색 결과 api 호출 err 처리

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -30,6 +30,11 @@ function IndexPage() {
       )
       .then((res) => {
         setSearchMovies(res.data);
+        console.log(res.data.length);
+      })
+      .catch((err) => {
+        setSearchMovies([]);
+        alert(err.response.data.error);
       });
   };
   return (
@@ -62,49 +67,51 @@ function IndexPage() {
           alignItems: 'center',
         }}
       >
-        {searchMovies.map((movie) => {
-          return searchMovies.length === 0 ? (
-            <div>검색 결과가 존재하지 않습니다.</div>
-          ) : (
-            <>
-              <Link
-                style={{
-                  textAlign: 'left',
-                  textDecoration: 'none',
-                  color: '#666',
-                  border: '1px solid #666',
-                  margin: '5px 0',
-                  width: '85%',
-                  padding: '0.5rem',
-                }}
-                to={`/movie/${movie.movieId}`}
-              >
-                <div style={{ padding: '1rem' }} key={movie.movieId}>
-                  <h4>제목: {movie.movieNm}</h4>
-                  <div>
-                    감독:
-                    {movie?.directors &&
-                      movie?.directors.map((dir) => {
-                        return `${dir.peopleNm}`;
-                      })}
+        {searchMovies.length === 0 ? (
+          <div style={{ color: 'green' }}>검색 결과가 존재하지 않습니다.</div>
+        ) : (
+          searchMovies.map((movie) => {
+            return (
+              <>
+                <Link
+                  style={{
+                    textAlign: 'left',
+                    textDecoration: 'none',
+                    color: '#666',
+                    border: '1px solid #666',
+                    margin: '5px 0',
+                    width: '85%',
+                    padding: '0.5rem',
+                  }}
+                  to={`/movie/${movie.movieId}`}
+                >
+                  <div style={{ padding: '1rem' }} key={movie.movieId}>
+                    <h4>제목: {movie.movieNm}</h4>
+                    <div>
+                      감독:
+                      {movie?.directors &&
+                        movie?.directors.map((dir) => {
+                          return `${dir.peopleNm}`;
+                        })}
+                    </div>
+                    <div>장르: {movie.genreAlt}</div>
+                    <div>
+                      출연 배우:{' '}
+                      {movie?.actors &&
+                        movie?.actors.map((actor) => {
+                          return `${actor.peopleNm} `;
+                        })}
+                    </div>
+                    <div>상영 시간: {movie.showTm} 분</div>
+                    <div>관람 등급: {movie.watchGradeNm}</div>
+                    <div>views: {movie.views}</div>
+                    <div>likes: {movie.likes}</div>
                   </div>
-                  <div>장르: {movie.genreAlt}</div>
-                  <div>
-                    출연 배우:{' '}
-                    {movie?.actors &&
-                      movie?.actors.map((actor) => {
-                        return `${actor.peopleNm} `;
-                      })}
-                  </div>
-                  <div>상영 시간: {movie.showTm} 분</div>
-                  <div>관람 등급: {movie.watchGradeNm}</div>
-                  <div>views: {movie.views}</div>
-                  <div>likes: {movie.likes}</div>
-                </div>
-              </Link>
-            </>
-          );
-        })}
+                </Link>
+              </>
+            );
+          })
+        )}
       </div>
     </div>
   );
